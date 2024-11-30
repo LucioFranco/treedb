a4316b5169d0d4c62d327554b3e30df90e872717
Lucio Franco <luciofranco14@gmail.com>
2024-11-30 16:58:22 -0500

    more page impl clean up

--- src/pager/page.rs
+++ src/pager/page.rs
@@ -1,15 +1,28 @@
 use std::{ptr::NonNull, rc::Rc};
 
-use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
+use zerocopy::{FromBytes, IntoBytes, KnownLayout};
 
 use crate::pager::VERSION;
 
 use super::PAGE_SIZE;
 
-// #[derive(Clone)]
-// pub struct Page {
-//     buf: NonNull<[u8]>,
-// }
+const PAGE_HEADER_SIZE: usize = std::mem::size_of::<PageHeader>();
+
+#[derive(FromBytes, IntoBytes, KnownLayout, Debug, Clone)]
+#[repr(C)]
+pub struct PageHeader {
+    checksum: u32,
+    version: u8,
+    page_type: u8,
+    _pad: u16,
+}
+
+#[derive(IntoBytes, Debug, Clone, PartialEq, Eq)]
+#[repr(C)]
+enum PageType {
+    Queue = 0,
+    Btree = 1,
+}
 
 #[derive(Debug)]
 pub struct PageBufMut {
@@ -31,9 +44,21 @@ impl PageBufMut {
 
         let (header, _) = PageHeader::mut_from_prefix(&mut buf[..]).unwrap();
 
-        header.version = VERSION as u16;
-        header.page_type = PageType::Queue as u16;
+        header.version = VERSION as u8;
+        header.page_type = 0;
         header.checksum = 0;
+    }
+
+    pub fn header_mut(&mut self) -> &mut PageHeader {
+        let header_len = size_of::<PageHeader>();
+
+        let buf = unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), header_len) };
+
+        PageHeader::mut_from_bytes(buf).unwrap()
+    }
+
+    pub fn get_usable_size(&self) -> usize {
+        PAGE_SIZE - size_of::<PageHeader>()
     }
 
     pub fn buf(&self) -> &[u8] {
@@ -85,103 +110,31 @@ impl PageBuf {
     }
 }
 
-#[derive(FromBytes, Immutable, Debug)]
-#[repr(C)]
-pub struct PageView<'a, T> {
-    header: PageHeader,
-    sub_header: T,
-    payload: &'a [u8],
-}
+// #[derive(FromBytes, Debug)]
+// #[repr(C)]
+// pub struct PageView<'a, T> {
+//     header: PageHeader,
+//     sub_header: T,
+//     payload: &'a [u8],
+// }
 
-#[derive(FromBytes, Immutable, Debug)]
-#[repr(C)]
-pub struct PageViewMut<'a, T> {
-    header: &'a mut PageHeader,
-    sub_header: &'a mut T,
-    payload: &'a mut [u8],
-}
+// #[derive(FromBytes, Debug)]
+// #[repr(C)]
+// pub struct PageViewMut<'a, T> {
+//     header: &'a mut PageHeader,
+//     sub_header: &'a mut T,
+//     payload: &'a mut [u8],
+// }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum PageType {
-    Queue = 0,
-    Btree = 1,
-}
+// #[derive(Debug, Clone, PartialEq, Eq)]
+// enum PageType {
+//     Queue = 0,
+//     Btree = 1,
+// }
 
-#[derive(FromBytes, IntoBytes, KnownLayout, Immutable, Debug, Clone, Default)]
-#[repr(C)]
-pub struct QueuePageHeader {
-    next_page_id: u64,
-    end_offset: u64,
-}
-
-#[derive(FromBytes, IntoBytes, KnownLayout, Immutable, Debug, Clone)]
-#[repr(C)]
-struct PageHeader {
-    version: u16,
-    page_type: u16,
-    checksum: u32,
-}
-
-// impl Page {
-//     pub fn init<T: IntoBytes + Immutable, A: Allocator>(
-//         alloc: &A,
-//         version: u32,
-//         sub_header: T,
-//     ) -> Page {
-//         let page_header = PageHeader {
-//             version,
-//             page_type: 0,
-//             checksum: 0,
-//         };
-
-//         let ptr = alloc
-//             .allocate(Layout::from_size_align(PAGE_SIZE, 8).unwrap())
-//             .unwrap();
-
-//         let mut page = Page { buf: ptr };
-
-//         let mut buf = unsafe { &mut page.buf.as_mut() };
-
-//         page_header.write_to_prefix(&mut buf).unwrap();
-//         let offset = std::mem::size_of::<PageHeader>();
-//         sub_header.write_to_prefix(&mut buf[offset..]).unwrap();
-
-//         page
-//     }
-
-//     pub fn view<T: FromBytes + Immutable>(&self) -> Option<PageView<'_, T>> {
-//         let buf = unsafe { self.buf.as_ref() };
-
-//         let (header, rest) = PageHeader::read_from_prefix(&buf).ok()?;
-//         let (sub_header, payload) = T::read_from_prefix(rest).ok()?;
-
-//         Some(PageView {
-//             header,
-//             sub_header,
-//             payload,
-//         })
-//     }
-
-//     pub fn view_mut<T: FromBytes + KnownLayout + IntoBytes>(
-//         &mut self,
-//     ) -> Option<PageViewMut<'_, T>> {
-//         let buf = unsafe { self.buf.as_mut() };
-
-//         let (header, rest) = PageHeader::mut_from_prefix(buf).ok()?;
-//         let (sub_header, payload) = T::mut_from_prefix(rest).ok()?;
-
-//         Some(PageViewMut {
-//             header,
-//             sub_header,
-//             payload,
-//         })
-//     }
-
-//     pub fn buf(&self) -> &[u8] {
-//         unsafe { self.buf.as_ref() }
-//     }
-
-//     pub fn buf_mut(&mut self) -> &mut [u8] {
-//         unsafe { self.buf.as_mut() }
-//     }
+// #[derive(FromBytes, IntoBytes, KnownLayout, Immutable, Debug, Clone, Default)]
+// #[repr(C)]
+// pub struct QueuePageHeader {
+//     next_page_id: u64,
+//     end_offset: u64,
 // }
